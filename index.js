ae01209fb04c045aebfef659b63b3d976b037bf3
Hera <mostafizhera@gmail.com>
2025-03-23 03:29:45 +0600

    get the plants details data

--- index.js
+++ index.js
@@ -114,6 +114,15 @@ async function run() {
       res.send(plants)
     })
 
+    // get plants details data =============>>>>>>>>>>
+
+    app.get('/plants/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) };
+      const result = await plantsCollection.findOne(query)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
     console.log(
